81d79f4e204a248a63184c2e128b8de7ddce431a
Ameizayoi <kevinjhernandez@outlook.com>
2019-01-27 12:34:52 -0400

    summit

---
+++ GGJ_GAME/Assets/ItemControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ItemControl : MonoBehaviour
+{
+    public AudioClip Sound;
+    private AudioSource source;
+    private float volLowRange = .5f;
+    private float volHighRange = 1.0f;
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+            this.gameObject.SetActive(false);
+            Destroy(gameObject);
+            source = GetComponent<AudioSource>();
+            float vol = Random.Range(volLowRange, volHighRange);
+            source.PlayOneShot(Sound, vol);
+        }
+    }
+}
---
+++ GGJ_GAME/Assets/videoplayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class videoplayer : MonoBehaviour
+{
+    public GameObject VideoPlayer;
+    public int timeToStop;
+
+    void Start()
+    {
+        VideoPlayer.SetActive(false);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            VideoPlayer.SetActive(true);
+            Destroy(VideoPlayer, timeToStop);
+        }
+    }
+}
